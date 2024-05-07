d362761b61126a8df1ed0297acb0c8f5e04aac1c
haoenlu <haoenlu.07@gmail.com>
2024-05-07 16:41:59 -0400

    update

--- data_utils.py
+++ data_utils.py
@@ -3,8 +3,8 @@ import torch
 
 def build_dataloader(config,args):
     numpy_data = np.load(args.data,allow_pickle=True).item()
-    train_data = numpy_data['data']
+    train_data = numpy_data['data'].transpose(0,2,1)
     dataloader = torch.utils.data.DataLoader(train_data,config['dataset']['batch_size'],shuffle=True)
-    _, feat , seq_len = train_data.shape
+    _, seq_len , feat = train_data.shape
     assert seq_len == config['model']['params']['seq_length'] and config['model']['params']['feature_size'] == feat
     return dataloader
