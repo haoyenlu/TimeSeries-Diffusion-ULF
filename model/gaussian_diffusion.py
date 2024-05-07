921ad71ed39b71aeab6ee519088e83b8a1c07107
haoenlu <haoenlu.07@gmail.com>
2024-05-07 17:34:19 -0400

    update

--- model/gaussian_diffusion.py
+++ model/gaussian_diffusion.py
@@ -261,7 +261,7 @@ class Diffusion_TS(nn.Module):
 
     def forward(self, x, **kwargs):
         b, c, n, device, feature_size, = *x.shape, x.device, self.feature_size
-        print(x.device)
+        print(x.type)
         assert n == feature_size, f'number of variable must be {feature_size}'
         t = torch.randint(0, self.num_timesteps, (b,), device=device).long()
         return self._train_loss(x_start=x, t=t, **kwargs)
