c62cf9fcf78de6251b2a8814d5152b19604ea82a
haoenlu <haoenlu.07@gmail.com>
2024-05-06 23:22:34 -0400

    update

---
+++ io_utils.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import yaml
+import json
+import torch
+import random
+import warnings
+import importlib
+import numpy as np
+
+
+def load_yaml_config(path):
+    with open(path) as f:
+        config = yaml.full_load(f)
+    return config
+
+def save_config_to_yaml(config, path):
+    assert path.endswith('.yaml')
+    with open(path, 'w') as f:
+        f.write(yaml.dump(config))
+        f.close()
+
+def instantiate_from_config(config):
+    if config is None:
+        return None
+    if not "target" in config:
+        raise KeyError("Expected key `target` to instantiate.")
+    module, cls = config["target"].rsplit(".", 1)
+    cls = getattr(importlib.import_module(module, package=None), cls)
+    return cls(**config.get("params", dict()))
