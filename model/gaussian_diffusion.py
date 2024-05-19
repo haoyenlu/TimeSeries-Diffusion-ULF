b372b4905a5183d065e6522d89a96cfcbb282a90
haoenlu <haoenlu.07@gmail.com>
2024-05-19 19:19:23 -0400

    update

--- model/gaussian_diffusion.py
+++ model/gaussian_diffusion.py
@@ -57,11 +57,10 @@ class Diffusion_TS(nn.Module):
         self.ff_weight = default(reg_weight, math.sqrt(self.seq_length) / 5)
         self.label_dim = label_dim
 
-        print(configs.keys())
         self.model = instantiate_from_config(configs['backbone'],
                                              n_feat=feature_size,n_channel=seq_length,
                                              label_dim=label_dim,max_len=seq_length,
-                                             mamba_config=configs['mamba'])
+                                             mamba_config=configs['backbone']['mamba'])
 
         if beta_schedule == 'linear':
             betas = linear_beta_schedule(timesteps)
