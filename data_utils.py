200696ae9446ff96f0f1e29b1966278b6f8cd93a
haoenlu <haoenlu.07@gmail.com>
2024-05-21 22:38:27 -0400

    update

--- data_utils.py
+++ data_utils.py
@@ -12,7 +12,7 @@ def build_dataloader(config,args):
 
         dataset = []
         for data,label in zip(numpy_data['data'],numpy_data['label']):
-            dataset.append([data.transpose(1,0),label.long()])
+            dataset.append([data.transpose(1,0),label.astype(int)])
     else:
         dataset = numpy_data['data'].transpose(0,2,1)
 
